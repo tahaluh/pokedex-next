de0fac3d43cf0eec14b2e042510465424c55883d
Thaua <thauanlucascpl@gmail.com>
2022-10-08 02:21:53 -0300

    about update

--- pages/about.js
+++ pages/about.js
@@ -6,7 +6,7 @@ export default function About() {
         <div className={styles.about}>
             <h1>Sobre o projeto</h1>
             <p>Esse projeto foi desenvolvido como objeto de estudo da framework Next.Js, onde o autor Thauã Magalhães, estudou e aplicou conceitos de rotas estáticas e dinâmicas por meio da framework a fim de desenvolver esta aplicação.</p>
-            <p>O canal "Hora de codar" foi utilizado como base de estudo</p>
+            <p>O canal "Hora de codar" foi utilizado como base de estudo.</p>
             <Image src="/images/mrmime.png" width="300" height="300" alt="Mr. Mime"/>
         </div>
     )
